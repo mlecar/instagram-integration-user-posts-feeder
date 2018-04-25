1ac273e98c51a60485e14f23e5791b55dcd096ad
Marcelo Lecar <marcelo.lecar@gmail.com>
2018-04-25 06:16:52 -0300

    Just trying Rome as rss

---
+++ src/main/java/com/mlc/instagram/user/RssGenerator.java
@@ -0,0 +1,30 @@
+package com.mlc.instagram.user;
+
+public class RssGenerator {
+
+    /*
+     * public SyndFeed generateSyndFeed() {
+     * 
+     * SyndFeed feed = new SyndFeedImpl(); feed.setFeedType("rss_2.0");
+     * 
+     * feed.setTitle("Instagram posts by " + userId);
+     * feed.setLink(instagramUserPage);
+     * feed.setDescription("Instagram posts by " + userId);
+     * 
+     * List<SyndEntry> entries = new ArrayList<>(); SyndEntry entry; SyndContent
+     * description;
+     * 
+     * entry = new SyndEntryImpl(); entry.setTitle(postText);
+     * entry.setLink(postLink); entry.setPublishedDate(postDate); description =
+     * new SyndContentImpl(); description.setType("text/plain");
+     * description.setValue("Initial release of ROME");
+     * entry.setDescription(description); entries.add(entry);
+     * 
+     * feed.setEntries(entries);
+     * 
+     * return feed;
+     * 
+     * }
+     */
+
+}
